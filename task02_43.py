e5a6437b34bc89661a08451a3560b6b4daa69530
fanur <makhiyanov.ff@gmail.com>
2022-09-24 11:22:27 +0300

    Список уникальных элементов заданной последовательности

--- task02_43.py
+++ task02_43.py
@@ -5,11 +5,17 @@
 
 # [1, 2, 3, 5, 1, 5, 3, 10] => [2, 10]
 
-# list = [1, 2, 3, 5, 1, 5, 3, 10]
-# unique_list = []
-# for i in list:
-#     if i > 1:
-#         list.remove(i)
-#     else:
-#         unique_list.append(i)
-# print(unique_list)
+some_list = [1, 2, 3, 5, 1, 5, 3, 10]
+
+def remove_repeated(my_list):
+    answer = {}
+    for i in my_list:
+        answer[i] = answer.get(i, 0) + 1
+    unique_list = []
+    for i in answer:
+        if answer[i] == 1:
+            unique_list.append(i)
+    return unique_list
+
+print(some_list)
+print(remove_repeated(some_list))
